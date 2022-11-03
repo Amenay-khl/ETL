af72c753887f3a84a304a2be9940a5156fb09689
l3miage-khiala <63787082+l3miage-khiala@users.noreply.github.com>
2022-11-03 16:47:00 +0100

    créate actor_sql_repository

--- src/sdv/infrastructure/sql/repositories/actor_sql_repository.py
+++ src/sdv/infrastructure/sql/repositories/actor_sql_repository.py
@@ -1,20 +1,24 @@
+from typing import List
+
+from requests import Session
+
+from sdv.domaine.entities.abstract_actor_entity import AbstractActorEntity
+from sdv.domaine.entities.actor import Actor
 from sdv.domaine.ports.ressources_repository import RessourcesRepository
+from sdv.infrastructure.sql.entities.actor_sql import ActorSQL
+
 
 class ActorSQLRepository(RessourcesRepository):
-    def __init__(self,engine):
-        self._engine=engine
+    def __init__(self, engine):
+        self._engine = engine
 
     def find_all(self) -> List[AbstractActorEntity]:
-        with Session-self._engine as session:
-            return (self.._create_entity(actor) for actor in session.query(ActorSQL))
+        with Session - self._engine as session:
+            return (self._create_entity(actor) for actor in session.query(ActorSQL))
         raise NotImplementedError
 
-
-    def find_id(self,ids:List[int])-> List[AbstractActorEntity]:
+    def find_by_id(self, ids: List[int]) -> List[AbstractActorEntity]:
         raise NotImplementedError
 
-
-    def createEntity
-
-
-
+    def create_entity(actor: ActorSQL) -> Actor:
+        return Actor(id=actor.id, first_name=actor.first_name, last_name=actor.last_name, last_update=actor.last_update)
